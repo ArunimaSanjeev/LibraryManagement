2603f9d31415552b8597a54d0dcf8a3b5a85645e
ArunimaSanjeev <arunimasanjeev25@gmail.com>
2026-03-06 10:36:19 +0530

    added library.py

---
+++ library.py
@@ -0,0 +1,12 @@
+print("Library management system")
+print("version-2")
+print("hi")
+print("Library Management System")
+
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "admin" and password == "1234":
+    print("Login Successful")
+else:
+    print("Invalid Login")
