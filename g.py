de58a90335296ac860fb84eca4def68bc52c2f2b
ArunimaSanjeev <arunimasanjeev25@gmail.com>
2026-03-06 14:08:52 +0530

    new commit

--- g.py
+++ g.py
@@ -37,3 +37,84 @@ print("git push origin v1.0")
 print("git clone https://github.com/username/repo.git")
 
 print("git checkout -b feature  - create and switch branch")
+
+'''Pipeline Task 1:
+pipeline {
+ agent any
+ stages {
+ stage('Checkout Code') {
+ steps {
+ git 'https://github.com/your-username/your-repo.git'
+ }
+ }
+ stage('Show Files') {
+ steps {
+ bat 'dir'
+ }
+ }
+ }
+}
+Pipeline Task 2:
+pipeline {
+ agent any
+ stages {
+ stage('Checkout Code') {
+ steps {
+ git 'https://github.com/your-username/your-repo.git'
+ }
+ }
+ stage('Print Directory') {
+ steps {
+ bat 'cd'
+ }
+ }
+ }
+}
+Pipeline task 3:
+pipeline {
+ agent any
+ stages {
+ stage('Checkout Code') {
+ steps {
+ git 'https://github.com/your-username/your-repo.git'
+ }
+ }
+ stage('Print Message') {
+ steps {
+ echo 'Hello! Jenkins Pipeline executed successfully'
+ }
+ }
+ }
+}
+Pipeline task 4:
+pipeline {
+ agent any
+ stages {
+ stage('Checkout Code') {
+ steps {
+ git 'https://github.com/your-username/your-repo.git'
+ }
+ }
+ stage('Create File') {
+ steps {
+ bat 'echo This file is created by Jenkins > demo.txt'
+ }
+ }
+ }
+}
+Pipeline task 5:
+pipeline {
+ agent any
+ stages {
+ stage('Checkout Code') {
+ steps {
+ git 'https://github.com/your-username/your-repo.git'
+ }
+ }
+ stage('Read File') {
+ steps {
+ bat 'type README.md'
+ }
+ }
+ }
+}'''
